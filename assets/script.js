c3fc0ebae34cf4d70de1247fe4fbb54805a52690
AlessioVisconti <alessiovisconti00@gmail.com>
2025-08-06 17:29:14 +0200

    little mod

--- assets/script.js
+++ assets/script.js
@@ -10,13 +10,13 @@ add.addEventListener("click", function () {
     const li = document.createElement("li"); // crea li
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox"; //creiamo la checkbox ovvero il quadrattino
-    const label = document.createElement("label"); //creiamo lo spazio dove poi verrà la stringa
-    label.textContent = taskStr + " "; // mettiamo stringa spazio per allontanare un po' la checkbox
+    const span = document.createElement("span"); //creiamo lo spazio dove poi verrà la stringa
+    span.textContent = taskStr + " "; // mettiamo stringa spazio per allontanare un po' la checkbox
     checkbox.addEventListener("change", function () {
       //aggiungiamo un evento a checkbox che si attiva quando il suo valore cambia
-      label.classList.toggle("sbarratura", checkbox.checked); //toggle aggiunge la classe solo se la condizione è vera(checkbox.checked)
+      span.classList.toggle("sbarratura", checkbox.checked); //toggle aggiunge la classe solo se la condizione è vera(checkbox.checked)
     });
-    li.appendChild(label);
+    li.appendChild(span);
     li.appendChild(checkbox);
     newTask.appendChild(li);
     textInput.value = ""; //riportiamo tutto al punto di partenza
